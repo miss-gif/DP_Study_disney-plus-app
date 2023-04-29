081936f04f00a5f442672c267ab8ce4a65296943
miss <ddc.akari@gmail.com>
2023-04-29 21:49:26 +0900

    Movie 모달

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,12 @@
 import axios from "../api/axios";
 import React, { useState, useEffect, useCallback } from "react";
 import "./Row.css";
+import MovieModal from "./MovieModal";
 
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
 
   const fetchMovieData = useCallback(async () => {
     const response = await axios.get(fetchUrl);
@@ -14,16 +17,34 @@ const Row = ({ title, id, fetchUrl }) => {
     fetchMovieData();
   }, [fetchMovieData]);
 
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
+
   return (
     <div>
       <h2>{title}</h2>
       <div className="slider">
         <div className="slider__arrow-left">
-          <span className="arrow">{"<"}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
         </div>
         <div id={id} className="row__posters">
           {movies.map((movie) => (
-            <img key={movie.id} className="row__poster" src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt={movie.name} />
+            <img
+              key={movie.id}
+              className="row__poster"
+              onClick={() => handleClick(movie)}
+              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              alt={movie.name}
+            />
           ))}
         </div>
         <div className="slider__arrow-right">
@@ -37,6 +58,7 @@ const Row = ({ title, id, fetchUrl }) => {
           </span>
         </div>
       </div>
+      {modalOpen && <MovieModal {...movieSelected} setModalOpen={setModalOpen}></MovieModal>}
     </div>
   );
 };
