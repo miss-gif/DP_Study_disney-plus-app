f5ab2d03888627283fad5c9eb1c78b2a9bd18cc2
miss <ddc.akari@gmail.com>
2023-05-01 06:13:17 +0900

    Swiper 적용

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 import "./MovieModal.css";
 
 const MovieModal = ({ backdrop_path, title, overview, name, release_date, first_air_date, vote_average, setModalOpen }) => {
+  const ref = useRef();
+
+  useOnClickOutside(ref, () => {
+    setModalOpen(false);
+  });
+
   return (
     <div className="presentation" role="presentation">
       <div className="wrapper-modal">
-        <div className="modal">
+        <div className="modal" ref={ref}>
           <span onClick={() => setModalOpen(false)} className="modal-close">
             X
           </span>
