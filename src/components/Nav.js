ec3b5a6ae7fa8ce16ec8c6178456b1f47fd5b092
miss <ddc.akari@gmail.com>
2023-05-02 19:42:03 +0900

    signOut 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import axios from "../api/axios";
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "firebase/auth";
 
 const Nav = () => {
   const [show, setShow] = useState(false);
@@ -12,6 +12,7 @@ const Nav = () => {
   const navigate = useNavigate();
   const auth = getAuth();
   const provider = new GoogleAuthProvider();
+  const [userData, setUserData] = useState({});
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -72,8 +73,22 @@ const Nav = () => {
 
   const handleAuth = () => {
     signInWithPopup(auth, provider)
-      .then((result) => {})
+      .then((result) => {
+        // console.log(result, "result");
+        setUserData(result.user);
+      })
       .catch((error) => console.log(error));
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUserData({});
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
@@ -90,13 +105,53 @@ const Nav = () => {
       {pathname === "/" ? (
         <Login onClick={handleAuth}>Login</Login>
       ) : (
-        <Input value={searchValue} onChange={handleChange} className="nav__input" type="text" placeholder="검색해주세요." />
+        <>
+          <Input value={searchValue} onChange={handleChange} className="nav__input" type="text" placeholder="검색해주세요." />
+
+          <SignOut>
+            <UserImg src="{userData.photoURL}" alt="{user.displayName}" />
+            <DropDown>
+              <span onClick={handleSignOut}>Sign Out</span>
+            </DropDown>
+          </SignOut>
+        </>
       )}
     </NavWrapper>
   );
 };
 
 export default Nav;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+`;
+
+const UserImg = styled.img`
+  border-radius: 50%;
+  width: 100%;
+  height: 100%;
+`;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 /50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100%;
+  opacity: 0;
+`;
 
 const Login = styled.a`
   background-color: rgba(0, 0, 0, 0.6);
